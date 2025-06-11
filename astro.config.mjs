734eea879fb5379491962a7a8f47bf99c7319970
yui192_ <44569789+yui19278@users.noreply.github.com>
2025-06-11 17:16:41 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://yui19278.github.io/",
